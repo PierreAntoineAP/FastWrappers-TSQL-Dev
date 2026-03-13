33b94cbc196b126a3ec879efe7fc6e3353b71bca
PierreAntoineAP <pierre-antoine.collet@architecture-performance.fr>
2026-03-13 16:44:18 +0100

    v0.5.2 - Ajout support trusted assembly pour SQL Server 2017+ (clr strict security)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -19,5 +19,5 @@
 //     Numéro de build
 //      Révision
 //
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
+[assembly: AssemblyVersion("0.5.2.0")]
+[assembly: AssemblyFileVersion("0.5.2.0")]
